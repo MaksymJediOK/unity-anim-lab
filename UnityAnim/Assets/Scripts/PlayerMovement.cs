65879e97dcd8945c0eeca1b35d4448c9e440ae7b
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2022-10-18 19:34:31 +0300

    moving platforms

--- UnityAnim/Assets/Scripts/PlayerMovement.cs
+++ UnityAnim/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
     private float speed = 5f;
-    private float jumpingPower = 16f;
+    [SerializeField] float jumpingPower = 8f;
     private bool isFacingRight = true;
+    private Animator animator;
+    private bool canDash = true;
+    private bool isDashing;
+    private float dashingPower = 20f;
+    private float dashingTime = 0.2f;
+    private float dashingCooldown = 1f;
+    [SerializeField] private TrailRenderer tr;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundcheck;
@@ -16,24 +23,46 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
+
         horizontal = Input.GetAxisRaw("Horizontal");
         if (Input.GetButtonDown("Jump") && isGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+
         }
+
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
 
+        };
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
         Flip();
+    }
+    void Run()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isWalking", playerHasHorizontalSpeed);
+    }
+    void Jump()
+    {
+        bool playerHasVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+        animator.SetBool("isJumping", playerHasVerticalSpeed);
     }
     private bool isGrounded()
     {
@@ -41,7 +70,13 @@ public class PlayerMovement : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+        if (isDashing)
+        {
+            return;
+        }
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        Jump();
+        Run();
     }
     private void Flip()
     {
@@ -53,4 +88,22 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = localScale;
         }
     }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
+    }
+
+
 }
